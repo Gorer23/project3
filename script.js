6c3e1e12c697ce0c1c29899521a018d819b66d23
Igor <igorkrsd093@gmail.com>
2024-01-29 12:42:39 +0200

    try

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict"; //только в начале,помогает ликвидировать ошибки в коде
+
+
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.querySelector('#current').parentNode.parentNode);// обращение к блоку выше
+// // ParentElement
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); // комбинировать кавычки
+//nextElementSibling следующий элемент
+
+
+for(let node of document.body.childNodes){
+    if(node.nodeName == '#text'){
+       continue;
+    }
+
+    console.log(node);
+}
+
